9efa93854699e810c7cd3e51aee1e9d6d2724d0e
Netanel Friedman <netanel.friedman6@gmail.com>
2024-02-04 17:06:59 +0200

    Initial commit

---
+++ 1/Interpreter.cpp
@@ -0,0 +1,29 @@
+#include "Type.h"
+#include "InterpreterException.h"
+#include "Parser.h"
+#include <iostream>
+
+#define WELCOME "Welcome to Magshimim Python Interperter version 1.0 by "
+#define YOUR_NAME "[Netanel Friedman]"
+
+int main(int argc, char** argv) {
+    std::cout << WELCOME << YOUR_NAME << std::endl;
+
+    std::string input_string;
+
+    // get new command from user
+    std::cout << ">>> ";
+    std::getline(std::cin, input_string);
+
+    while (input_string != "quit()") {
+        // parsing command
+        Parser::parseString(input_string);
+
+
+        // get new command from user
+        std::cout << ">>> ";
+        std::getline(std::cin, input_string);
+    }
+
+    return 0;
+}
---
+++ 1/Parser.cpp
@@ -0,0 +1,15 @@
+#include "Parser.h"
+#include <iostream>
+
+
+Type* Parser::parseString(std::string str)
+{
+	if (str.length() > 0)
+	{
+		std::cout << str << std::endl;
+	}
+
+	return nullptr;
+}
+
+
